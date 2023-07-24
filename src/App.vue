4a31239614211b730eb3a3eadda3161ce6ab16ae
zhoujian <zhou.j-neu@neusoft.com>
2023-07-24 14:40:47 +0800

    feat: 去掉复制功能

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@
             <van-button
               class="body__content__subtitle"
               icon="notes-o"
-              @click="copyText('toady')"
+              @click="printInfo('toady')"
               >复制今日工作内容</van-button
             >
           </div>
@@ -61,7 +61,7 @@
             <van-button
               class="body__content__subtitle"
               icon="notes-o"
-              @click="copyText('week')"
+              @click="printInfo('week')"
               >复制本周工作内容</van-button
             >
           </div>
@@ -116,8 +116,7 @@ export default Vue.extend({
       // eslint-disable-next-line no-undef
       J2C.webViewReload()
     },
-    copyText(type) {
-      // 数字没有 .length 不能执行selectText 需要转化成字符串
+    printInfo(type) {
       let titles = []
       if (type === 'toady') {
         this.calendarEventInfo.todayData.forEach((item, index) => {
@@ -134,42 +133,6 @@ export default Vue.extend({
       console.log(titles)
       const textString = titles.join('\n')
       console.log(textString)
-      let input = document.querySelector('#copy-input')
-      if (!input) {
-        input = document.createElement('input')
-        input.id = 'copy-input'
-        input.readOnly = 'readOnly' // 防止ios聚焦触发键盘事件
-        input.style.position = 'absolute'
-        input.style.left = '-1000px'
-        input.style.zIndex = '-1000'
-        document.body.appendChild(input)
-      }
-      input.value = textString
-      // ios必须先选中文字且不支持 input.select();
-      selectText(input, 0, textString.length)
-      console.log(document.execCommand('copy'), 'execCommand')
-      if (document.execCommand('copy')) {
-        document.execCommand('copy')
-        alert('已复制到粘贴板')
-      }
-      input.blur()
-
-      // input自带的select()方法在苹果端无法进行选择，所以需要自己去写一个类似的方法
-      // 选择文本。createTextRange(setSelectionRange)是input方法
-      function selectText(textbox, startIndex, stopIndex) {
-        if (textbox.createTextRange) {
-          // ie
-          const range = textbox.createTextRange()
-          range.collapse(true)
-          range.moveStart('character', startIndex) // 起始光标
-          range.moveEnd('character', stopIndex - startIndex) // 结束光标
-          range.select() // 不兼容苹果
-        } else {
-          // firefox/chrome
-          textbox.setSelectionRange(startIndex, stopIndex)
-          textbox.focus()
-        }
-      }
     }
   }
 })
